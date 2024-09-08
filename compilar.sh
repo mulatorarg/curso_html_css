a376e46568c93ca4ff542a461d3de80c76a55f34
mulator <campogabriel@gmail.com>
2024-09-07 23:26:27 -0300

    mas correcciones

--- compilar.sh
+++ compilar.sh
@@ -1,6 +1,6 @@
 echo "Compilando styles de Home..."
-sass "scss\home.scss" "css\home.css"
+sass "scss\home.scss" "css\home.css" --style compressed
 echo "Compilando styles de Pages..."
-sass "scss\pages.scss" "css\pages.css"
+sass "scss\pages.scss" "css\pages.css" --style compressed
 
 echo "Listo"
